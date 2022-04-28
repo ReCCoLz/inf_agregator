c7bc9ed0e413327a18ef31e9f5c7d07581aff1b5
ReCCoLz <recollectionwindscribe@gmail.com>
2022-04-28 17:51:01 +0300

    ebat

--- test.py
+++ test.py
@@ -1,15 +1,25 @@
-import pymorphy2
+from time import sleep
 import requests, re
+from requests import ConnectionError
 from fake_useragent import UserAgent
 from lxml import html
+import string
+import wordcloud
+from nltk import word_tokenize, FreqDist
+from nltk.corpus import stopwords
+import nltk
 
+
+rus_stopwords = stopwords.words('russian')
 urls = {
     'https://www.interfax.ru/news/' : '<a href=\"/\w+/\d{6,}\"><h3>\D+</h3>',
     'https://ria.ru/': '<span class=\"cell-list__item-title\">\D+/span>',
     'https://news.mail.ru/politics/': '<span class=\"newsitem__title-inner\">\D+</span>',
     'https://www.rbc.ru/politics/?utm_source=topline':'<span class=\"item__title rm-cm-item-text\"\D+</span>',
-    'https://rg.ru/news.html' : '<span class=\"b-link__inner-text\">\D+</span>'
+    'https://rg.ru/news.html' : '<span class=\"b-link__inner-text\">\D+</span>',
 }
+
+
 
 def get_data():
     ua = UserAgent()
@@ -29,7 +39,10 @@ def get_data():
         'TE': 'trailers' }
 
     for url in urls.items():
-        r = requests.get(url[0], headers=headers)
+        try:
+            r = requests.get(url[0], headers=headers)
+        except ConnectionError:
+            continue
         # print(r.text)
         match = re.findall(url[1], r.text)
         if 'h3' in match[0]:
@@ -43,10 +56,33 @@ def get_data():
                 text = tree.cssselect('span:first_child') # костыль для получения текста из под тега html
                 fild_data.append(text[0].text_content().replace('\xa0', ' ').replace('\u200b', '').strip())
         if 'div' in match[0]:
+            for data in match[0]:
                 tree = html.fromstring(data)
                 text = tree.cssselect('div:first_child') # костыль для получения текста из под тега html
                 fild_data.append(text[0].text_content().strip())
     return fild_data
 
-print(len(get_data()))
-print(get_data())
+
+for text in get_data():
+    f = open('data_news.txt', encoding='utf-8')
+    all_data = [i.rstrip() for i in f.readlines()]
+    f.close()
+    if text not in all_data:
+        with open('data_news.txt', 'a', encoding='utf-8') as file:
+            file.write(text)
+            file.write('\n')
+            file.write('\n') 
+
+f = open('data_news.txt', "r", encoding='utf-8')
+text=f.read()
+text = text.lower()
+spec_chars = string.punctuation + '\n'
+text = "".join([ch for ch in text if ch not in spec_chars])
+
+text_tokens = word_tokenize(text)
+
+text = nltk.Text(text_tokens)
+fdist = FreqDist(text)
+remove_sw = [word for word in text_tokens if not word in stopwords.words()]
+
+print(remove_sw.most_common(5))
