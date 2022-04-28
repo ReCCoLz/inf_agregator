727285a0bfd6ae5cc571bc623d3af082969fecb0
AlexVorobushek <alex080605@mail.ru>
2022-04-28 21:38:47 +0300

    rename functions, create substidution decorator

--- меняю.py
+++ меняю.py
@@ -7,6 +7,7 @@ import re
 import os
 import time
 import pymorphy2
+import json
 
 morph = pymorphy2.MorphAnalyzer()
 
@@ -28,8 +29,23 @@ CLEAR_COMMAND: str = {
     'posix': 'clear'
 }.get(os.name)
 
+def substidution(func):
+    file_name = func.__name__+'.txt'
+    def f(*args, **kwargs):
+        if not TEST:
+            return func(*args, **kwargs)
+        if not os.path.exists(file_name):
+            output = func(*args, **kwargs)
+            with open(file_name, 'w') as file:
+                file.write(json.dumps(output))
+            return output
+        with open(file_name) as f:
+            return json.loads(f.read())
+    return f
 
-def clear_str(s1, li):
+
+
+def clear_str(s1:str, li:list) -> str:
     s2 = s1
     for iii in li:
         s2 = s2.replace(iii, '')
@@ -40,7 +56,7 @@ class Word:
     def __init__(self, name) -> None:
         self.name = name
 
-    def get_status(self, lastd: dict) -> str:
+    def define_status(self, lastd: dict) -> str:
         if self.name not in lastd:
             self.status = 'NEW'
         else:
@@ -57,9 +73,9 @@ class Words:
             :LOOKAT]
         self.all = [Word(word[0]) for word in words]
 
-    def get_status(self, lastd: dict) -> None:
+    def define_words_status(self, lastd: dict) -> None:
         for word in self.all:
-            word.get_status(lastd)
+            word.define_status(lastd)
 
     def __getitem__(self, key: int):
         return self.all[key]
@@ -70,12 +86,8 @@ class News:
         self.rawtitles = []
         self.all = self.get_al()
 
-    def get_al(self):  # долгая функция
-        # не хочу  долго ждать, поэтому подставляю уже когда-то выданные этой функцией данные
-        if TEST:
-            with open('News.get_al.output.txt') as f:
-                return f.read()
-
+    @substidution
+    def get_al(self) -> str:
         al = ''
         for i in URLIST.keys():
             s = self.parce(i, URLIST[i])
@@ -83,8 +95,6 @@ class News:
                 s = self.parce(i, URLIST[i])
             if s != 'FIASCO':
                 al += s + ' '
-        # with open('News.get_al.output.txt', 'w') as f:
-        #     f.write(clear_str(al, CLEAR_LIST))
         return clear_str(al, CLEAR_LIST)
 
     def parce(self, url, regexp):
@@ -130,7 +140,7 @@ class Data:
     def __init__(self, words: Words) -> None:
         self.read_last()
         self.words = words
-        self.words.get_status(self.lastd)
+        self.words.define_words_status(self.lastd)
 
     def read_last(self) -> dict:
         try:
@@ -193,14 +203,9 @@ while True:
     # os.system(CLEAR_COMMAND)
     print(data)
     data.write_to_file()
-
     # time.sleep(77)
-
-    # Определяем самое поднявшееся слово
     grouwth_word = data.get_grouwth_word()
     print(f'Рост {grouwth_word.status} показало слово "{grouwth_word.name}"')
-
-    # печатаем новости со словом, показавшим рост
     for new in news.find_news_by_word(grouwth_word.name, 6):
         print(new)
 
