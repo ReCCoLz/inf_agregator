2a7aa6cac75d0e92d7c13b5cf1b49845ef653c28
AlexVorobushek <alex080605@mail.ru>
2022-05-17 00:07:07 +0300

    tuniing, new bot answers

--- main_bot.py
+++ main_bot.py
@@ -24,10 +24,9 @@ async def start(message: types.Message):
 async def help(message: types.Message):
     await bot.send_message(message.from_user.id, f'Все, что тебе нужно нажимать, дружок, это /get.')
 
-# @dp.message_handler(commands=['get'])
-# async def get(message: types.Message):
-#     print(message.from_user)
-#     await bot.send_message(message.from_user.id, f'иди в жопу, ты мне мешаешь, я сейчас общаюсь не с тобой. как меня допилят, тебе сообщат')
+@dp.message_handler(commands=['new_get'])
+async def get(message: types.Message):
+    await bot.send_message(message.from_user.id, main())
 
 @dp.message_handler(commands=['get'])
 async def get(message: types.Message):
@@ -50,7 +49,6 @@ async def get(message: types.Message):
 
 "Мишустин исключил Римаса Туминаса из лауреатов премии в области культуры"'''
     await bot.send_message(message.from_user.id, m)
-    await bot.send_message(message.from_user.id, main())
 
 if __name__ == '__main__':
     executor.start_polling(dp)
@@ -69,3 +67,40 @@ if __name__ == '__main__':
 # Рост NEW показало слово "мишустин"
 # Мишустин лишил режиссера Туминаса правительственной премии
 # Мишустин исключил Римаса Туминаса из лауреатов премии в области культуры
+
+
+
+# № [перемещение] слово1 [0] РОССИЯ
+# 2 [__+1] УКРАИНА
+# 3 [__+2] ПУТИН
+# 4 [__+3] ФИНЛЯНДИЯ
+# 5 [__+4] МИШУСТИН
+# 6 [__+5] ТУМИНАСА
+# 7 [__+6] ПРЕМИЯ
+# 8 [__+7] БОРРЕЛЬ
+# 9 [__+8] ЗЕРНО
+# 10[__+9] ХРАНИЛИЩЕ
+
+
+# Рост +9 показало слово "хранилище"
+# Боррель: ЕС поможет Украине вывезти зерно из хранилищ
+# Боррель: ЕС поможет Украине опустошить хранилища зерна
+
+
+
+# № [перемещение] слово1 [0] РОССИЯ
+# 2 [+1] УКРАИНА
+# 3 [+2] ПУТИН
+# 4 [+3] ФИНЛЯНДИЯ
+# 5 [+4] МИШУСТИН
+# 6 [+5] ТУМИНАСА
+# 7 [+6] ПРЕМИЯ
+# 8 [+10] ПОМОЩЬ
+# 9 [+11] РОССИЯНИН
+# 10 [+12] ТЫСЯЧА
+
+# Рост +12 показало слово "тысяча"
+
+# "Из опасных районов ДНР, ЛНР и Украины эвакуировали еще более 13 тысяч человек"
+
+# "ЦБ разрешил россиянам переводить до 50 тысяч долларов за рубеж"
